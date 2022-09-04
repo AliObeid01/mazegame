e09752d870180c383e92ae31d113b9688221d6e3
unknown <aliiobeid01@gmail.com>
2022-09-04 16:09:52 +0300

    adding timer

--- game.js
+++ game.js
@@ -6,8 +6,10 @@ var start = document.getElementById("start");
 var end = document.getElementById("end");
 var status = document.getElementById("status");
 var scoreDiv = document.querySelector("div.boundary.example");
-var score=0;
 var user;
+var score=0;
+
+var timer;
 
 start.onmouseover = function() {mouseOverStart()};
 end.onmouseover = function() {mouseOverEnd()};
@@ -28,30 +30,44 @@ redDiv[4].style.backgroundColor = "red";
   scoreDiv.style.textAlign = "center";
 });
 }
+
 function mouseOverStart() {
 
    for (let i = 0; i < redDiv.length; i++) {
    redDiv[i].style.backgroundColor = "#eeeeee";
  }
-  status.innerHTML = "Start or press S to reset.";
-
+  
+  	
 }
 
-function onclickStart() {
 
+
+function onclickStart() {
+ var time=0;
  for (let i = 0; i < redDiv.length; i++) {
    redDiv[i].style.backgroundColor = "#eeeeee";
  } 
-   status.innerHTML = "Lets Start again";
+   
    scoreDiv.innerHTML= score=0;
    scoreDiv.style.textAlign = "center";
+   
+   timer = setInterval(function(){
+     
+    if(time == 59){
+      clearInterval(timer);
+       
+    }
+     
+    status.innerHTML= time+=1;
+}, 1000);
+
    
    
 }
 
 function mouseOverEnd() {
 
-   status.innerHTML = "YOU WIN:) SCORE +100";
+   status.innerHTML = "YOU WIN:) SCORE +10";
    scoreDiv.innerHTML= score+=10;
    scoreDiv.style.textAlign = "center";
    localStorage.setItem("scoreSaved", score);
@@ -63,7 +79,7 @@ function userName() {
   localStorage.setItem("username", user);
   if (user == localStorage.getItem("username")) {
     status.innerHTML =
-    "Hello " + localStorage.getItem("username") + "! Your last Score is " + localStorage.getItem("scoreSaved", score)+ "! Try to beat it ;) ";
+    "Hello " + localStorage.getItem("username") + "! Your last Score is " + localStorage.getItem("scoreSaved", score)+ "! Try to beat it ;) "+"<br> Press S to Start.You have just 1 minute";
   }}
 
    
