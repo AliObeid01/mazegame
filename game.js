de4a66da34b79a9c66fcfd8b23f26e7c7d2ae237
unknown <aliiobeid01@gmail.com>
2022-09-04 12:57:58 +0300

    fix hit wall color

--- game.js
+++ game.js
@@ -1,25 +1,33 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-var redDiv = document.querySelectorAll(".boundary");
+
+var redDiv = document.getElementsByClassName("boundary");
 var start = document.getElementById("start");
 var end = document.getElementById("end");
 var status = document.getElementById("status");
 var scoreDiv = document.querySelector("div.boundary.example");
 var score=0;
 
-redDiv.onmouseover = function() {mouseOverWall()};
+
 start.onmouseover = function() {mouseOverStart()};
 end.onmouseover = function() {mouseOverEnd()};
 start.onclick = function() {onclickStart()};
 
-function mouseOverWall() {
+for (let i = 0; i < redDiv.length; i++){
 
-  status.innerHTML = "YOU LOSE SCORE -5:(";
+redDiv[i].addEventListener('mouseover', function handleMouseOver() {
+    
+redDiv[0].style.backgroundColor = "red";
+redDiv[1].style.backgroundColor = "red";
+redDiv[2].style.backgroundColor = "red";
+redDiv[3].style.backgroundColor = "red";
+redDiv[4].style.backgroundColor = "red";
+
+  status.innerHTML = "YOU LOSE:( SCORE -5";
   scoreDiv.innerHTML= score-=5;
   scoreDiv.style.textAlign = "center";
-
+});
 }
-
 function mouseOverStart() {
 
    for (let i = 0; i < redDiv.length; i++) {
@@ -42,7 +50,7 @@ function onclickStart() {
 
 function mouseOverEnd() {
 
-   status.innerHTML = "YOU WIN SCORE +10 :)";
+   status.innerHTML = "YOU WIN:) SCORE +10";
    scoreDiv.innerHTML= score+=10;
    scoreDiv.style.textAlign = "center";
 
